e1144b4e2b54d05ae101f9dbdee4292d6e5ee725
Jin Kim <jinwoov@gmail.com>
2020-03-16 20:57:21 -0700

    Finished Main method

---
+++ ExceptionalHandling/ExceptionalHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExceptionalHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StartSeuence();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Your Error was {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Your application is finish!");
+            }
+        }
+    }
+}
