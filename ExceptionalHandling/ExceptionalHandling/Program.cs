a251af10c1ae3d17d506c100759f3bb8c13e4fe2
Jin Kim <jinwoov@gmail.com>
2020-03-16 21:09:08 -0700

    Added try catch for start sequence

--- ExceptionalHandling/ExceptionalHandling/Program.cs
+++ ExceptionalHandling/ExceptionalHandling/Program.cs
@@ -23,18 +23,30 @@ namespace ExceptionalHandling
 
         static void StartSequence()
         {
-            Console.WriteLine("Enter a value that is greater than zero(0)");
-            int userValue = Convert.ToInt32(Console.ReadLine());
-            int[] valueArray = new int[userValue];
-            // To populate
-            Populate(userArray);
-            // To capture sum of array
-            GetSum(userArray);
-            // To capture product of array
-            GetProduct(userArray);
-            // To capture quotient of array
-            GetQuotient(userArray);
-
+            try
+            {
+                Console.WriteLine("Enter a value that is greater than zero(0)");
+                int userValue = Convert.ToInt32(Console.ReadLine());
+                int[] valueArray = new int[userValue];
+                // To populate
+                Populate(userArray);
+                // To capture sum of array
+                GetSum(userArray);
+                // To capture product of array
+                GetProduct(userArray);
+                // To capture quotient of array
+                GetQuotient(userArray);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("You had formatting error");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("You had overflow error please put exact value for its type");
+            }
+           
         }
+
     }
 }
