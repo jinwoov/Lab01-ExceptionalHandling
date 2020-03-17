dd4ad0cf894bfa74c9ac504ce6abefcc39b446ac
Jin Kim <jinwoov@gmail.com>
2020-03-16 21:06:42 -0700

    StartSequence metho is set

--- ExceptionalHandling/ExceptionalHandling/Program.cs
+++ ExceptionalHandling/ExceptionalHandling/Program.cs
@@ -20,5 +20,21 @@ namespace ExceptionalHandling
                 Console.WriteLine("Your application is finish!");
             }
         }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Enter a value that is greater than zero(0)");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            int[] valueArray = new int[userValue];
+            // To populate
+            Populate(userArray);
+            // To capture sum of array
+            GetSum(userArray);
+            // To capture product of array
+            GetProduct(userArray);
+            // To capture quotient of array
+            GetQuotient(userArray);
+
+        }
     }
 }
