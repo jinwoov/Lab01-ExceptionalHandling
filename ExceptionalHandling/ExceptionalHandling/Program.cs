f68b8ec02fe829c1abc9d7deb1bf75549f54c390
Jin Kim <jinwoov@gmail.com>
2020-03-16 21:23:44 -0700

    Finished GetSum method

--- ExceptionalHandling/ExceptionalHandling/Program.cs
+++ ExceptionalHandling/ExceptionalHandling/Program.cs
@@ -31,7 +31,7 @@ namespace ExceptionalHandling
                 // To populate
                 int[] populatedArray = Populate(valueArray);
                 // To capture sum of array
-                GetSum(valueArray);
+                int totalValue = GetSum(populatedArray);
                 // To capture product of array
                 GetProduct(valueArray);
                 // To capture quotient of array
@@ -49,16 +49,31 @@ namespace ExceptionalHandling
 
         static int[] Populate(int[] numArray)
         {
+            // storing input from user
             string userResponse;
+            // Running the for loop to save each response depending on the length of an array
             for (int i = 0; i < numArray.Length; i++)
             {
                  Console.WriteLine("Please enter numerical value!");
                  userResponse = Console.ReadLine();
                  numArray[i] = Convert.ToInt32(userResponse);
             }
-
+            // Returning the value
             return numArray;
+        }
 
+        static int GetSum(int[] numArray)
+        {
+            int sum = 0;
+            foreach (int values in numArray)
+            {
+                sum += values;
+            }
+
+            if (sum < 20)
+                throw new Exception("Value of {sum} is too low");
+
+            return sum;
         }
     }
 }
